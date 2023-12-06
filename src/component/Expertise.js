a1a5a266ea27c84488c2660cf9fd71cfffbce231
me-sahilraut <136321316+me-sahilraut@users.noreply.github.com>
2023-12-06 13:14:31 +0530

    text change

--- src/component/Expertise.js
+++ src/component/Expertise.js
@@ -8,7 +8,9 @@ const Expertise = () => {
             < div className='text-center mt-[6.25rem] ' >
                 <h1 className=' text-3xl md:text-4xl text-white font-bold pt-[12rem] uppercase '>Area of Expertise</h1></div>
             <p className='mt-3 text-white text-center text-lg  mx-[3rem] lg:mx-[18rem] mb-5 '>
-                GrowCite excels in strategic recruitment, inclusive hiring, and leveraging technology to match top-tier talent with diverse industry needs.
+                Crafting careers with skilled precision,
+                Client-centric focus, our mission's decision.
+                Expertise unfolds, a professional vision.
             </p>
             <div className='flex flex-wrap flex-col md:flex-row  gap-5 md:justify-center  items-center mb-10 mx-auto md:mx-[1rem] pb-[8rem] pt-[5rem] '>
 
